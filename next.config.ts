861b61c34f0e4d2317c28c3bc5b819af661e5ea6
Rae <239801273+hellrae@users.noreply.github.com>
2025-11-13 11:00:49 +0530

    add: basic structure

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/saas-starter",
+        destination: "https://github.com/hellrae/saas-starter",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
